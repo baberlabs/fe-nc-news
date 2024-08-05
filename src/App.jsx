79614881ad195240b4039c8d02d0b44bb6ae4a93
Baber Khan <bbrkhnn@outlook.com>
2024-08-05 09:45:39 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <section className="flex min-h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-2xl font-black">Northcoders News</h1>
+      <p className="">Made by Baber Khan</p>
+    </section>
+  );
+}
