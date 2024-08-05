a264b67f30d3a79004c577840eed7105602d944c
Baber Khan <bbrkhnn@outlook.com>
2024-08-05 14:19:12 +0100

    Create global context for loggedInUser

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { LoggedInUserProvider } from "./contexts/LoggedInUserProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <LoggedInUserProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </LoggedInUserProvider>,
 );
