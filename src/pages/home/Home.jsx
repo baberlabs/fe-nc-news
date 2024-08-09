0a41b18491f54cb5c550e504aa0c45d4d21190b5
Baber Khan <bbrkhnn@outlook.com>
2024-08-09 14:36:15 +0100

    Create a minimal home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,43 @@
+import { Link } from "react-router-dom";
+
+import { AccessTimeFilled, Comment, ThumbUp } from "@mui/icons-material";
+
 export default function Home() {
   return (
-    <section>
-      <h2>Home</h2>
+    <section className="flex flex-col px-4 py-8 text-center">
+      <h2 className="text-[8rem] font-black tracking-tighter text-red-600">
+        NN
+      </h2>
+      <p className="text-xl">News from farthest reaches of the world</p>
+      <div className="mt-8 flex flex-row flex-wrap justify-center gap-4">
+        <ArticleLink
+          text="Most Votes"
+          href="/articles?sort_by=votes"
+          icon={<ThumbUp style={{ fontSize: "5rem" }} />}
+        />
+        <ArticleLink
+          text="Most Comments"
+          href="/articles?sort_by=comment_count"
+          icon={<Comment style={{ fontSize: "5rem" }} />}
+        />
+        <ArticleLink
+          text="Most Recent"
+          href="/articles?sort_by=created_at"
+          icon={<AccessTimeFilled style={{ fontSize: "5rem" }} />}
+        />
+      </div>
     </section>
+  );
+}
+
+function ArticleLink({ icon, text, href }) {
+  return (
+    <Link
+      to={href}
+      className="flex w-40 flex-col items-center justify-center gap-2 rounded bg-gray-300 p-4 hover:bg-zinc-800 hover:font-bold hover:text-red-600"
+    >
+      {icon}
+      <p>{text}</p>
+    </Link>
   );
 }
